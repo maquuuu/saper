c85883869784e28907dac8b6237172bfc0ce7682
maquuuuu <jmateusz323@gmail.com>
2025-01-21 23:35:15 +0100

    Flagi i reakcja na komendy

--- mapa.c
+++ mapa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "dzialanie.h"
 
 #define LATWY_WIERSZE 9
 #define LATWY_KOLUMNY 9
@@ -53,11 +54,26 @@ void generujMape(int wiersze, int kolumny) {
 
     wypiszMape(mapa, wiersze, kolumny);
 
+    int x, y;
+    char akcja;
+    while (1) {
+        printf("Podaj ruch (f x y - flaga, r x y - odkrycie): ");
+        scanf(" %c %d %d", &akcja, &x, &y);
+
+        if (akcja == 'q') {
+            break;
+        }
+
+        wykonajRuch(mapa, wiersze, kolumny, x, y, akcja);
+        wypiszMape(mapa, wiersze, kolumny);
+    }
+
     for (int i = 0; i < wiersze; i++) {
         free(mapa[i]);
     }
     free(mapa);
 }
+
 
 void wypiszMape(char **mapa, int wiersze, int kolumny) {
     for (int i = 0; i < wiersze; i++) {
