d7d901144c43d00a530e926a34563ae0d4ccb12d
maquuuu <jmateusz323@gmail.com>
2025-01-26 11:56:31 +0100

    Delete dzialanie.h

--- dzialanie.h
+++
@@ -1,9 +0,0 @@
-#ifndef DZIELANIE_H
-#define DZIALANIE_H
-
-void wykonajRuch(char **mapa, int wiersze, int kolumny, int x, int y, char akcja);
-void ustawFlage(char **mapa, int x, int y);
-void odkryjPole(char **mapa, int x, int y);
-
-#endif
-
