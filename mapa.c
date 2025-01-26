f958febeeb9699794ca6b5ec01f9a2535a177247
maquuuuu <jmateusz323@gmail.com>
2025-01-26 12:23:09 +0100

    Poprawki w planszy

--- mapa.c
+++ mapa.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "wynik.h"  
+#include "wynik.h"
 
 #define LATWY_WIERSZE 9
 #define LATWY_KOLUMNY 9
@@ -9,7 +9,6 @@
 #define SREDNI_KOLUMNY 16
 #define TRUDNY_WIERSZE 16
 #define TRUDNY_KOLUMNY 30
-
 
 #define LATWY_MINY 10
 #define SREDNI_MINY 40
@@ -20,6 +19,7 @@ void wypiszMape(char **mapa, int wiersze, int kolumny, int koniecGry);
 void losujMiny(char **mapa, int wiersze, int kolumny, int x, int y, int miny);
 int liczMiny(char **mapa, int wiersze, int kolumny, int x, int y);
 void odkryjPole(char **mapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odsłonietePola);
+void rozszerzOdkrycie(char **mapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odsłonietePola);
 void wykonajRuch(char **mapa, int wiersze, int kolumny, int x, int y, char akcja, int *koniecGry, int *odsłonietePola);
 void ustawFlage(char **mapa, int x, int y);
 
@@ -56,17 +56,17 @@ void generujMape(int wiersze, int kolumny, int miny, int poziomTrudnosci) {
     for (int i = 0; i < wiersze; i++) {
         mapa[i] = malloc(kolumny * sizeof(char));
         for (int j = 0; j < kolumny; j++) {
-            mapa[i][j] = '.';  
+            mapa[i][j] = '.';
         }
     }
 
-    wypiszMape(mapa, wiersze, kolumny, 0);  
+    wypiszMape(mapa, wiersze, kolumny, 0);
 
     int x, y;
     char akcja;
     int pierwszeOdkrycie = 1;
-    int koniecGry = 0;  
-    int odsłonietePola = 0;  
+    int koniecGry = 0;
+    int odsłonietePola = 0;
 
     while (!koniecGry) {
         printf("Podaj ruch (f x y - flaga, r x y - odkrycie): ");
@@ -78,7 +78,6 @@ void generujMape(int wiersze, int kolumny, int miny, int poziomTrudnosci) {
 
         if (akcja == 'r') {
             if (pierwszeOdkrycie) {
-               
                 losujMiny(mapa, wiersze, kolumny, x, y, miny);
                 pierwszeOdkrycie = 0;
             }
@@ -89,7 +88,6 @@ void generujMape(int wiersze, int kolumny, int miny, int poziomTrudnosci) {
         }
 
         wypiszMape(mapa, wiersze, kolumny, koniecGry);
-        
         obliczWynik(odsłonietePola, poziomTrudnosci);
     }
 
@@ -99,11 +97,9 @@ void generujMape(int wiersze, int kolumny, int miny, int poziomTrudnosci) {
 void wypiszMape(char **mapa, int wiersze, int kolumny, int koniecGry) {
     for (int i = 0; i < wiersze; i++) {
         for (int j = 0; j < kolumny; j++) {
-            
             if (koniecGry && mapa[i][j] == 'M') {
                 printf("M ");
             } else if (mapa[i][j] == 'M') {
-                
                 printf(". ");
             } else {
                 printf("%c ", mapa[i][j]);
@@ -122,9 +118,8 @@ void losujMiny(char **mapa, int wiersze, int kolumny, int x, int y, int miny) {
         int mx = rand() % wiersze;
         int my = rand() % kolumny;
 
-        
         if (mapa[mx][my] != 'M' && !(mx == x && my == y)) {
-            mapa[mx][my] = 'M';  
+            mapa[mx][my] = 'M';
             minyDoRozmieszczenia--;
         }
     }
@@ -133,7 +128,6 @@ void losujMiny(char **mapa, int wiersze, int kolumny, int x, int y, int miny) {
 int liczMiny(char **mapa, int wiersze, int kolumny, int x, int y) {
     int liczbaMin = 0;
 
-    // Sprawdzamy pola
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
             int nx = x + i;
@@ -148,19 +142,38 @@ int liczMiny(char **mapa, int wiersze, int kolumny, int x, int y) {
     return liczbaMin;
 }
 
+void rozszerzOdkrycie(char **mapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odsłonietePola) {
+    if (x < 0 || x >= wiersze || y < 0 || y >= kolumny || mapa[x][y] != '.') {
+        return;
+    }
+
+    int liczbaMin = liczMiny(mapa, wiersze, kolumny, x, y);
+    mapa[x][y] = '0' + liczbaMin;
+
+    (*odsłonietePola)++;
+
+    if (liczbaMin == 0) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int nx = x + i;
+                int ny = y + j;
+
+                if (nx >= 0 && nx < wiersze && ny >= 0 && ny < kolumny && mapa[nx][ny] == '.') {
+                    rozszerzOdkrycie(mapa, wiersze, kolumny, nx, ny, koniecGry, odsłonietePola);
+                }
+            }
+        }
+    }
+}
+
 void odkryjPole(char **mapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odsłonietePola) {
     if (mapa[x][y] == '.') {
-        
-        int liczbaMin = liczMiny(mapa, wiersze, kolumny, x, y);
-        mapa[x][y] = '0' + liczbaMin;  
-        (*odsłonietePola)++;  
+        rozszerzOdkrycie(mapa, wiersze, kolumny, x, y, koniecGry, odsłonietePola);
     } else if (mapa[x][y] == 'F') {
-        
         printf("Pole oznaczone flagą!\n");
     } else if (mapa[x][y] == 'M') {
-        *koniecGry = 1;  
+        *koniecGry = 1;
     } else {
-        
         printf("To pole jest już odkryte!\n");
     }
 }
@@ -169,16 +182,16 @@ void wykonajRuch(char **mapa, int wiersze, int kolumny, int x, int y, char akcja
     if (akcja == 'r') {
         odkryjPole(mapa, wiersze, kolumny, x, y, koniecGry, odsłonietePola);
     } else if (akcja == 'f') {
-        ustawFlage(mapa, x, y);  
+        ustawFlage(mapa, x, y);
     }
 }
 
 void ustawFlage(char **mapa, int x, int y) {
     if (mapa[x][y] == '.') {
-        mapa[x][y] = 'F';  
+        mapa[x][y] = 'F';
         printf("Flaga ustawiona na polu (%d, %d)\n", x, y);
     } else if (mapa[x][y] == 'F') {
-        mapa[x][y] = '.';  
+        mapa[x][y] = '.';
         printf("Flaga usunięta z pola (%d, %d)\n", x, y);
     } else {
         printf("To pole jest już odkryte lub zawiera minę, nie można ustawić flagi.\n");
