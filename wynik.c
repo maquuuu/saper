66dad2e238bcdf14a491c1f391073ce6edf7baf8
jmjmjmjmjmjmjmjmjm <01197836@pw.edu.pl>
2025-01-26 20:14:06 +0100

    .

--- wynik.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// Funkcja do obliczania wyniku
-void obliczWynik(int odsłoniętePola, int poziomTrudnosci) {
-    int wynik = odsłoniętePola * poziomTrudnosci;
-    printf("Aktualny wynik: %d\n", wynik);
-}
-
