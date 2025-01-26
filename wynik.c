8e768788a85c875503d2f4b913a2e33155420188
maquuuuu <jmateusz323@gmail.com>
2025-01-26 11:43:22 +0100

    Poprawki i dzialajace miny i zliczanie wyniku

---
+++ wynik.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Funkcja do obliczania wyniku
+void obliczWynik(int odsłoniętePola, int poziomTrudnosci) {
+    int wynik = odsłoniętePola * poziomTrudnosci;
+    printf("Aktualny wynik: %d\n", wynik);
+}
+
