d3973ef8cb4f7b281a47afccca02c6c99662109f
jmjmjmjmjmjmjmjmjm <01197836@pw.edu.pl>
2025-01-27 12:46:42 +0100

    niepotrzebna zmienna

--- interak.h
+++ interak.h
@@ -3,8 +3,7 @@
 
 int liczMiny(char **mapa, int wiersze, int kolumny, int x, int y);
 void odkryjPole(char **mapa, char**widocznaMapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odslonietePola);
-void rozszerzOdkrycie(char **mapa, char**widocznaMapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odslonietePola);
+void rozszerzOdkrycie(char **mapa, int wiersze, int kolumny, int x, int y, int *koniecGry, int *odslonietePola);
 void ustawFlage(char **mapa, char **widocznaMapa, int x, int y);
 
 #endif
-
